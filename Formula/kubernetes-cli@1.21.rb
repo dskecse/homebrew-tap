bbf95d80a3114ded4f54e8e08e4f8990ee4d61e8
Dzianis Dashkevich <dskecse@gmail.com>
2022-01-17 17:37:34 +0300

    Update kubernetes-cli 1.21 formula

--- Formula/kubernetes-cli@1.21.rb
+++ Formula/kubernetes-cli@1.21.rb
@@ -1,5 +1,5 @@
 class KubernetesCliAT121 < Formula
-  desc "Kubernetes command-line interface (1.21)"
+  desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
       tag:      "v1.21.8",
@@ -8,14 +8,16 @@ class KubernetesCliAT121 < Formula
 
   livecheck do
     url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    regex(/^v?(1\.21(?:\.\d+)+)$/i)
   end
 
   keg_only :versioned_formula
 
-  deprecate! date: "2021-04-18", because: :versioned_formula
+  # https://kubernetes.io/releases/patch-releases/#1-21
+  deprecate! date: "2022-04-28", because: :versioned_formula
 
   depends_on "bash" => :build
+  depends_on "coreutils" => :build
   depends_on "go" => :build
 
   uses_from_macos "rsync" => :build
@@ -25,20 +27,25 @@ class KubernetesCliAT121 < Formula
     rm_rf ".brew_home"
 
     # Make binary
+    # Deparallelize to avoid race conditions in creating symlinks, creating an error like:
+    #   ln: failed to create symbolic link: File exists
+    # See https://github.com/kubernetes/kubernetes/issues/106165
+    ENV.deparallelize
+    ENV.prepend_path "PATH", Formula["coreutils"].libexec/"gnubin" # needs GNU date
     system "make", "WHAT=cmd/kubectl"
     bin.install "_output/bin/kubectl"
 
     # Install bash completion
-    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "bash")
+    output = Utils.safe_popen_read(bin/"kubectl", "completion", "bash")
     (bash_completion/"kubectl").write output
 
     # Install zsh completion
-    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "zsh")
+    output = Utils.safe_popen_read(bin/"kubectl", "completion", "zsh")
     (zsh_completion/"_kubectl").write output
 
     # Install man pages
     # Leave this step for the end as this dirties the git tree
-    system "hack/generate-docs.sh"
+    system "hack/update-generated-docs.sh"
     man1.install Dir["docs/man/man1/*.1"]
   end
 
@@ -47,13 +54,10 @@ class KubernetesCliAT121 < Formula
     assert_match "kubectl controls the Kubernetes cluster manager.", run_output
 
     version_output = shell_output("#{bin}/kubectl version --client 2>&1")
-
     assert_match "GitTreeState:\"clean\"", version_output
-
     if build.stable?
-      assert_match stable.instance_variable_get(:@resource)
-                         .instance_variable_get(:@specs)[:revision],
-                   version_output
+      revision = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision]
+      assert_match revision.to_s, version_output
     end
   end
 end
