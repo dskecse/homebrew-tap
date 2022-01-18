abfcb667816560c634d4aeeeb1e812107e4333d7
Dzianis Dashkevich <dskecse@gmail.com>
2022-01-18 14:50:56 +0300

    Make `kubectl@1.21` download binary rather than compile from source

--- Formula/kubernetes-cli@1.21.rb
+++ Formula/kubernetes-cli@1.21.rb
@@ -1,43 +1,19 @@
 class KubernetesCliAT121 < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
-  url "https://github.com/kubernetes/kubernetes/archive/v1.21.8.tar.gz"
-  sha256 "b585d37fb145de9b91cc934669e64ffd0743f40298a2de970509ac182c70a67a"
+  version "v1.21.8"
+  url "https://dl.k8s.io/release/#{version}/bin/darwin/amd64/kubectl"
+  sha256 "f4b95b0fd01137d9976aa3c617b3a0f02204123ff06160988af1db4b2c813b27"
   license "Apache-2.0"
-  head "https://github.com/kubernetes/kubernetes.git"
 
   # https://kubernetes.io/releases/patch-releases/#1-21
   deprecate! date: "2022-04-28", because: "enters maintenance mode"
 
-  depends_on "bash" => :build
-  depends_on "coreutils" => :build
-  depends_on "go" => :build
-
-  uses_from_macos "rsync" => :build
-
   def install
-    # Don't dirty the git tree
-    rm_rf ".brew_home"
-
-    # Make binary
-    # Deparallelize to avoid race conditions in creating symlinks, creating an error like:
-    #   ln: failed to create symbolic link: File exists
-    # See https://github.com/kubernetes/kubernetes/issues/106165
-    ENV.deparallelize
-    ENV.prepend_path "PATH", Formula["coreutils"].libexec/"gnubin" # needs GNU date
-    system "make", "WHAT=cmd/kubectl"
-    bin.install "_output/bin/kubectl"
+    bin.install Dir["bin/*"]
   end
 
   test do
-    run_output = shell_output("#{bin}/kubectl 2>&1")
-    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
-
-    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
-    assert_match "GitTreeState:\"clean\"", version_output
-    if build.stable?
-      revision = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision]
-      assert_match revision.to_s, version_output
-    end
+    system "#{bin}/kubectl", "version", "--client"
   end
 end
